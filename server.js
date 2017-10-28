ca6e6cea6932d74e1bb0da330b1aaefb749dbca6
J C <j.e.calderon@gmail.com>
2017-10-28 10:17:25 -0400

    added packages

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
