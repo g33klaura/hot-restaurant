58efe5701ce50fcf3d62987de13c723caae77b29
J C <j.e.calderon@gmail.com>
2017-10-28 10:39:40 -0400

    added folders

--- server.js
+++ server.js
@@ -6,3 +6,28 @@ var path = require("path");
 // =============================================================
 var app = express();
 var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//initial objects:
+var current = [
+   
+    {
+        id:'1',
+        name: 'Jane Q Public',
+        phone: '407-555-5555',
+        email: 'jane@public.me'
+    }   
+];
+
+var waiting = [
+   
+    {
+        id:'5',
+        name: 'John Q Public',
+        phone: '407-555-5556',
+        email: 'john@public.me'
+    }   
+];
